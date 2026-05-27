8f663c5c27a6239be61acd3455cf633cbb790af6
Arigela-Surya-Ganesh <ganesh.hntc@gmail.com>
2026-05-27 19:48:43 +0530

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,34 @@
+#1.Area of Square
+#Question: Calculate the area of a square. - Formula: Area = side × side - Input: - Side = 5 - Output: - Area of square is: 25
+Area = 5*5
+print("Area of squre is : " + str(Area))
+
+#2.Area of Rectangle
+#Question: Calculate the area of a rectangle. - Formula: Area = length × breadth - Input: - Length = 6 - Breadth = 4 - Output: - Area of rectangle is: 24
+length = 6
+breadth = 4
+area = length * breadth
+print("Area of rectangle is: " + str(area))
+
+#3.Area of Triangle
+#Question: Calculate the area of a triangle using base and height. - 
+#Formula: Area = (1/2) × base × height - Input: - Base = 8 - Height = 5 - Output: - Area of triangle is: 20.0
+base = 8
+height = 5
+area = 1/2 * base * height
+print("Area of Triangle " + str(area))
+
+#4.Perimeter of Square
+#Question: Calculate the perimeter of a square. - Formula: Perimeter = 4 × side - Input: - Side = 6 - Output: - Perimeter of square is: 24
+side = 6
+perimeter = 4*side
+print("Perimeter of Square " + str(perimeter))
+
+
+#5.Perimeter of Rectangle
+#Question: Calculate the perimeter of a rectangle. - 
+#Formula: Perimeter = 2 × (length + breadth) - Input: - Length = 5 - Breadth = 3 - Output: - Perimeter of rectangle is: 16
+length = 5
+breadth = 3
+perimeter = 2 * (length + breadth)
+print("Perimeter of Rectangle " + str(perimeter))
